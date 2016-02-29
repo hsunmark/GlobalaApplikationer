6db887c376c44f7fa0924e2a0a5f38f31c317b52
Adam <adamekb@hotmail.com>
2016-02-29 17:37:32 +0100

    asd

--- src/main/java/controller/RecruitmentController.java
+++ src/main/java/controller/RecruitmentController.java
@@ -25,14 +25,16 @@ public class RecruitmentController {
     private String USER_REGEX = "^[a-zA-Z0-9]+$";
     private String SSN_REGEX = "^[0-9]+$";
     private String PW_REGEX = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+
     /**
      * checks that the person trying to login are using a valid combination of
      * username and password
+     *
      * @param username
      * @param password
      * @return
      */
-    public boolean login(String username, String password, RecruitmentManager manager){
+    public boolean login(String username, String password, RecruitmentManager manager) {
         try {
             Collection<PersonEntity> getUser = em.createNamedQuery("PersonEntity.findByUsername")
                     .setParameter("username", username).getResultList();
@@ -44,8 +46,9 @@ public class RecruitmentController {
             return false;
         } catch (Exception e) {
             manager.setMessage("Database error");
+        }
 
-        if(validateLoginParameters(username, password)){
+        if (validateLoginParameters(username, password)) {
             try {
                 personEntity = em.find(PersonEntity.class, username);
                 if (personEntity != null && personEntity.getPassword().equals(password)) {
@@ -53,25 +56,24 @@ public class RecruitmentController {
                 }
                 manager.setMessage("invalid username or password");
                 return false;
-            } catch (Exception e) {
+            } catch (Exception e1) {
                 manager.setMessage("Database error");
                 return false;
             }
-        }
-        else{
+        } else {
             manager.setMessage("Login parameters not valid");
-
             return false;
         }
     }
 
     /**
      * registers a user account and persists it in database
+     *
      * @param registerDTO
      */
     public boolean register(RegisterDTO registerDTO, RecruitmentManager manager) {
 
-        if(validateRegisterParameters(registerDTO, manager)){
+        if (validateRegisterParameters(registerDTO, manager)) {
             Collection<PersonEntity> usernameCheck = em.createNamedQuery("PersonEntity.findByUsername")
                     .setParameter("username", registerDTO.getUsername()).getResultList();
 
@@ -86,15 +88,14 @@ public class RecruitmentController {
                             registerDTO.getSsn(), registerDTO.getEmail(), registerDTO.getUsername(),
                             registerDTO.getPassword());
                     em.persist(personEntity);
-                } catch (Exception e){
+                } catch (Exception e) {
                     return false;
                 }
             } else {
                 manager.setMessage("Username taken");
                 return false;
             }
-        }
-        else{
+        } else {
             manager.setMessage("registration failed due to invalid paramters");
             return false;
         }
@@ -103,41 +104,45 @@ public class RecruitmentController {
     }
 
     //method that validates login parametrs 
-    private boolean validateLoginParameters(String loginName, String loginPw) { 
-        if (loginPw.equals("") || loginName.equals("")) { 
-            return false; 
+    private boolean validateLoginParameters(String loginName, String loginPw) {
+        if (loginPw.equals("") || loginName.equals("")) {
+            return false;
         }
          if (!loginPw.matches(PW_REGEX) || !loginName.matches(USER_REGEX)) {
-                     return false;
+            return false;
         } 
         return true; 
-    }  
+    }
+
     //method that validates register parameters 
     private boolean validateRegisterParameters(RegisterDTO registerDTO, RecruitmentManager manager) {
-        if (registerDTO.getUsername().equals("") 
-            || registerDTO.getPassword().equals("") 
-            || registerDTO.getFirstname().equals("") 
-            || registerDTO.getLastname().equals("") 
-            || registerDTO.getRole().equals("") 
-            || registerDTO.getSsn().equals("") 
-            || registerDTO.getEmail().equals("")){ 
-            return false; 
-        } 
-        if (registerDTO.getPassword().length() < 6) { 
-            return false; 
-        }  
-        if (!registerDTO.getUsername().matches(USER_REGEX) 
+        if (registerDTO.getUsername().equals("")
+                || registerDTO.getPassword().equals("")
+                || registerDTO.getFirstname().equals("")
+                || registerDTO.getLastname().equals("")
+                || registerDTO.getRole().equals("")
+                || registerDTO.getSsn().equals("")
+                || registerDTO.getEmail().equals("")) {
+            return false;
+        }
+
+        if (registerDTO.getPassword().length() < 6) {
+            return false;
+        }
+        if (!registerDTO.getUsername().matches(USER_REGEX)
                 || !registerDTO.getPassword().matches(PW_REGEX)
-                || !registerDTO.getFirstname().matches(NAME_REGEX) 
-                || !registerDTO.getLastname().matches(NAME_REGEX)) { 
-            return false; 
-        } 
-        if (!manager.isValidEmailAddress(registerDTO.getEmail())) { 
-            return false; 
+                || !registerDTO.getFirstname().matches(NAME_REGEX)
+                || !registerDTO.getLastname().matches(NAME_REGEX)) {
+            return false;
+        }
+
+
+        if (!manager.isValidEmailAddress(registerDTO.getEmail())) {
+            return false;
         }  
         if ((!registerDTO.getSsn().matches(SSN_REGEX) || (registerDTO.getSsn().length() != 10))) {
-                     return false; 
-        } 
-        return true; 
+             return false; 
+        }
+        return true;
     }
 }
